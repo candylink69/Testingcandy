aed25abe8549615f64f5676a87f351df5248569f
216120306076 <omomshanti079@gmail.com>
2026-04-14 14:14:03 +0530

    Update thumbs.js

--- thumbs.js
+++ thumbs.js
@@ -8,6 +8,22 @@ const thumbnails = {
     "FF001": "https://i.postimg.cc/KzSS39yw/FF001.webp",
     "FF002": "https://i.postimg.cc/QtGGK6rn/FF002.webp",
     "FF003": "https://i.postimg.cc/V6qyqjYh/FF003.webp",
+    "FM001": "https://i.postimg.cc/d12M28qb/FM001.webp",
+    "FU001": "https://i.postimg.cc/gJV9VvYQ/FU001.webp",
+    "HM001": "https://i.postimg.cc/RF7r7cSr/HM001.webp",
+    "HM002": "https://i.postimg.cc/ZR8k8rbb/HM002.webp",
+    "HM003": "https://i.postimg.cc/50BcBw4j/HM003.webp",
+    "I001":  "https://i.postimg.cc/6qtkYyq9/I001.webp",
+    "I002":  "https://i.postimg.cc/C5S3mz55/I002.webp",
+    "I003":  "https://i.postimg.cc/x8nwtc8N/I003.webp",
+    "I004":  "https://i.postimg.cc/vTbJ3cT9/I004.webp",
+    "I005":  "https://i.postimg.cc/1XskJfXG/I005.webp",
+    "I006":  "https://i.postimg.cc/mkBv8tkQ/I006.webp",
+    "I007":  "https://i.postimg.cc/Vvg3Tm0M/I007.webp",
+    "I008":  "https://i.postimg.cc/Vvg3Tm0B/I008.webp",
+    "I009":  "https://i.postimg.cc/vThpjbx3/I009.webp",
+    "I0010":  "https://i.postimg.cc/d3wb4L3t/I0010.webp",
+    "I0011":  "https://i.postimg.cc/C5S3mz5K/I0011.webp",
 };
 
 function getThumbnailUrl(videoId) {
