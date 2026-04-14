a84b66bd400001dc13a09623e98dec2ec9802dd1
216120306076 <omomshanti079@gmail.com>
2026-04-14 12:44:45 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,332 @@
+const videos = [
+  {
+    id: "V001",
+    title: "",
+    duration: "43:53",
+    categories: ["V000"],
+    preview: "https://candylink69.embed4me.vip/s_gEKNH3mF1jgnB_JmHmyw/x68/r8czn6n1/hduqgl/preview.webp"
+  },
+  {
+    id: "V002",
+    title: "",
+    duration: "50:04",
+    categories: ["V000"],
+    preview: "https://candylink69.embed4me.vip/9asvksR8YOv_5deOJigESg/urp/fmglyzca/jwusob/preview.webp"
+  },
+  {
+    id: "V003",
+    title: "",
+    duration: "45:58",
+    categories: ["V000"],
+    preview: "https://candylink69.embed4me.vip/wgUK0l8R4QoSiKwlSR6RWw/xq/j3tyqzox/pqdcic/preview.webp"
+  },
+  {
+    id: "V004",
+    title: "",
+    duration: "1:01:34",
+    categories: ["V000"],
+    preview: "https://candylink69.embed4me.vip/rEsM1ZLl-0W2X0YA_KvOtQ/miy/j3tyqzox/bled5e/preview.webp"
+  },
+  {
+    id: "I001",
+    title: "",
+    duration: "19:34",
+    categories: ["I"],
+    preview: "https://candylink69.embed4me.vip/l32_EfA9UJPteokTGwc74w/q3o/j3tyqzox/id9j9y/preview.webp"
+  },
+  {
+    id: "M001",
+    title: "",
+    duration: "29:46",
+    categories: ["mom"],
+    preview: "https://candylink69.embed4me.vip/ZlAnuh1s0rhAIvE4_yUypw/miy/afyhwq5f/l8a8la/preview.webp"
+  },
+  {
+    id: "S001",
+    title: "Stepbrother thinks his sister is a lesbian but she proves him wrong",
+    duration: "42:04",
+    categories: ["sister"],
+    preview: "https://candylink69.embed4me.vip/yVrianvT4NxThVw044mVEg/mf/fmg9y9ce/nwg6r1/preview.webp"
+  },
+  {
+    id: "I002",
+    title: "",
+    duration: "11:22",
+    categories: ["I"],
+    preview: "https://candylink69.embed4me.vip/Fkwieh6e54jU9lP7MmuRYQ/luy/fmg9y9ce/q1ihd1/preview.webp"
+  },
+  {
+    id: "S002",
+    title: "",
+    duration: "33:41",
+    categories: ["sister"],
+    preview: "https://candylink69.embed4me.vip/gQtkVVt9k94c9jcdSjsCuQ/m9/fmg9y9ce/3bslyx/preview.webp"
+  },
+  {
+    id: "M002",
+    title: "",
+    duration: "59:19",
+    categories: ["mom"],
+    preview: "https://candylink69.embed4me.vip/UDe34V5-SMOLqvohBVfBeA/qu/5z1ivqbn/6dadqt/preview.webp"
+  },
+  {
+    id: "S003",
+    title: "",
+    duration: "34:16",
+    categories: ["sister"],
+    preview: "https://candylink69.embed4me.vip/NJyoSwTGgoo6EuEPYCou9A/6s/5z1ivqbn/qfpfz3/preview.webp"
+  },
+  {
+    id: "FU001",
+    title: "",
+    duration: "54:00",
+    categories: ["free-use"],
+    preview: "https://candylink69.embed4me.vip/O3m5GLtC1lHH-H4g32Kd4w/tab/5z1ivqbn/qfp1qo/preview.webp"
+  },
+  {
+    id: "FF001",
+    title: "",
+    duration: "18:45",
+    categories: ["fantasy-flesh"],
+    preview: "https://candylink69.embed4me.vip/XSs7T0mGj46UzRUvAfHMoQ/hz/lodt93ey/6d3jfr/preview.webp"
+  },
+   {
+    id: "FF002",
+    title: "",
+    duration: "11:17",
+    categories: ["fantasy-flesh"],
+    preview: "https://candylink69.embed4me.vip/QSyt22CBLmZ4S_9AzYtt7Q/ek/dq8zrme1/b9zcb1/preview.webp"
+   },
+   {
+    id: "SW001",
+    title: "",
+    duration: "55:48",
+    categories: ["swapping"],
+    preview: "https://candylink69.embed4me.vip/aADK6alye1wXIz7NqLv-Uw/xq/ghsr6zxu/561dqj/preview.webp"
+   },
+  {
+    id: "FM001",
+    title: "",
+    duration: "21:32",
+    categories: ["FM"],
+    preview: "https://candylink69.embed4me.vip/Jn1F0RP77tHpvF-gRIXC-w/6s/el9qhvk6/n1wk3n/preview.webp"
+  },
+  {
+    id: "Movie001",
+    title: "",
+    duration: "01:44:57",
+    categories: ["Full-Movies"],
+    preview: "https://candylink69.embed4me.vip/OgX7MY9REy7iMoJYI1eG3A/bgm/yg6kbbwm/ejjxu5/preview.webp"
+  },
+  {
+    id: "HM001",
+    title: "Enbo Taboo Charming Mother",
+    duration: "02:49:30",
+    categories: ["HM"],
+    preview: "https://candylink69.embed4me.vip/d1jZJK_19LFM0bCyGDP0mQ/is9/puo3likr/vip1n8/preview.webp"
+  },
+  {
+    id: "Movie002",
+    title: "Playing D&D games",
+    duration: "01:56:14",
+    categories: ["Full-Movies"],
+    preview: "https://candylink69.embed4me.vip/NGTrfhTy2Mz0zLAPTH6uwA/js/9dr5ku6k/pcwk3g/preview.webp"
+  },
+  {
+    id: "M003",
+    title: "Son seduced his Mom",
+    duration: "29:46",
+    categories: ["mom"],
+    preview: "https://candylink69.embed4me.vip/PlxLd7yTFkNpuOjHzNvzig/n8l/afyhwv5e/3ml6hd/preview.webp"
+  },
+  {
+    id: "S004",
+    title: "Sister help her brother",
+    duration: "42:27",
+    categories: ["sister"],
+    preview: "https://candylink69.embed4me.vip/vZUlr6MmCbq_WUl04ja8DA/m9/afyhwv5e/6r58a3/preview.webp"
+  },
+  {
+    id: "Movie003",
+    title: "Tera Patrick Forbidden Tales 2001 full Movie",
+    duration: "01:23:12",
+    categories: ["Full-Movies"],
+    preview: "https://candylink69.embed4me.vip/kjUHOYYMEGN-TtREIJLXwQ/ic/r8c1njny/ejwm3p/preview.webp"
+  },
+  {
+    id: "FF003",
+    title: "Bad Latina Pranks These Are Not Two Enormous Pumpkins These Are Juicy Holes",
+    duration: "39:46",
+    categories: ["fantasy-flesh"],
+    preview: "https://candylink69.embed4me.vip/bYxuuXlfkFlswoiJWX1Iyw/ty/r8c1njny/pcy9fe/preview.webp"
+  },
+  {
+    id: "I003",
+    title: "Girlfriend Boyfriend Oyo Room Mms Video Viral",
+    duration: "13:00",
+    categories: ["I"],
+    preview: "https://candylink69.embed4me.vip/hnABuPdVzqASag7ZtrPaVQ/m9/v1feadni/oa6evf/preview.webp"
+  },
+  {
+    id: "I004",
+    title: "Hot Indian Girl Full Hard Fucking Video With Blowjob",
+    duration: "06:37",
+    categories: ["I"],
+    preview: ""
+  },
+  {
+    id: "I005",
+    title: "Viral Indian Beauty Girl Mms In Car",
+    duration: "04:10",
+    categories: ["I"],
+    preview: ""
+  },
+  {
+    id: "I006",
+    title: "Gf Ko Ghr Bulake Choda",
+    duration: "03:55",
+    categories: ["I"],
+    preview: ""
+  },
+  {
+    id: "I007",
+    title: "Hot Big Boobs Mallu GF Fucking Collection 20 Video Viral Mms Porn",
+    duration: "02:24",
+    categories: ["I"],
+    preview: ""
+  },
+  {
+    id: "I008",
+    title: "Teen Sex Mms Cousin Bhai Sath Night Me",
+    duration: "02:37",
+    categories: ["I"],
+    preview: ""
+  },
+  {
+    id: "I009",
+    title: "Beautiful Arab College Girls Pink Pussy Licked And Blowjob To Bf",
+    duration: "04:13",
+    categories: ["I"],
+    preview: ""
+  },
+  {
+    id: "I0010",
+    title: "Hidden MMS Truck Driver Viral Roadside Romance Sex Caught By Stranger",
+    duration: "03:44",
+    categories: ["I"],
+    preview: ""
+  },
+  {
+    id: "I0011",
+    title: "Couple having sex in hotel",
+    duration: "03:10",
+    categories: ["I"],
+    preview: ""
+  },
+    {
+    id: "M004",
+    title: "The son fucks his mother together with his father",
+    duration: "47:49",
+    categories: ["mom"],
+    preview: "https://candylink69.embed4me.vip/U8hDAiZg1ZOGBlvD_58k4Q/np/v1feadni/xu5jb5/preview.webp"
+    },
+  {
+    id: "B001",
+    title: "The boss fucking his virgin babysitter",
+    duration: "41:14",
+    categories: ["Boss"],
+    preview: "https://candylink69.embed4me.vip/WlCJ8w1sPNxgnqwavgUPvw/ek/ojqadwss/xiojo6/preview.webp"
+  },
+  {
+    id: "S005",
+    title: "A sister was offering advice to help her brother overcome his addiction, but she ended up getting fucked by him herself.",
+    duration: "32:15",
+    categories: ["sister"],
+    preview: "https://candylink69.embed4me.vip/utNpxua_kDNg7uXIBTB7bw/il/krnvuiu1/moajsb/preview.webp"
+  },
+  {
+    id: "HM002",
+    title: "Yokorenbo:immoral mother",
+    duration: "56:23",
+    categories: ["HM"],
+    preview: "https://candylink69.embed4me.vip/ttuEx47mfXWpYEW3BKdJaw/9ow/yg6kbkwo/bkfqpp/preview.webp"
+  },
+  {
+    id: "Movie004",
+    title: "Cinderella XXX An Axel Braun Parody Full Movie",
+    duration: "1:55:45",
+    categories: ["Full-Movies"],
+    preview: "https://candylink69.embed4me.vip/1A9sA0A3xTi3H8PUspCrFg/mik/xieomuss/mowlqx/preview.webp"
+  },
+  {
+    id: "HM003",
+    title: "Modaete yo,Adam-kun(Adam's Sweet Agony)",
+    duration: "45:12",
+    categories: ["HM"],
+    preview: "https://candylink69.embed4me.vip/DDspeH0fKJwJ1w4No2RxQQ/ho/b6iuxu93/cx8hsb/preview.webp"
+  },
+  {
+    id: "D001",
+    title: "The daughter gets fucked by her father like pretending her mother.",
+    duration: "54:21",
+    categories: ["D"],
+    preview: "https://candylink69.embed4me.vip/LvA-2E0OEtas8su_LNIZoQ/sc/r8c1npnq/3m3pvs/preview.webp"
+  },
+  {
+    id: "O001",
+    title: "The waitress fucks the chef, a coworker, and a customer in the restaurant.",
+    duration: "40:10",
+    categories: ["O"],
+    preview: "https://candylink69.embed4me.vip/NVWgnzzFHOXhtdFzeTEhfA/mik/wybszpq5/nfqpvo/preview.webp"
+  },
+  {
+    id: "BM001",
+    title: "The brother-in-law blackmailed his sister-in-law and fuck on her wedding day.",
+    duration: "37:49",
+    categories: ["BM"],
+    preview: "https://candylink69.embed4me.vip/UiLyovWAsORdzf2ieAMkvQ/hld/puo3lcki/xoizn9/preview.webp"
+  },
+  {
+    id: "B002",
+    title: "Housemaid Teasing Her Boss And Paid The Price For It",
+    duration: "28:09",
+    categories: ["Boss"],
+    preview: "https://candylink69.embed4me.vip/s0yPvwjVmSCoMo3cWt975g/x68/ipljeq8a/8xatpf/preview.webp"
+  },
+  {
+    id: "Movie005",
+    title: "Heroes of Might and Magic Armagedons Blade in the Black Ladys Chamber",
+    duration: "2:34:48",
+    categories: ["Full-Movies"],
+    preview: "https://candylink69.embed4me.vip/QNqDW39GVDIepGh03qyJmg/vz/xieomnsk/r1tty1/preview.webp"
+  },
+  {
+    id: "BS001",
+    title: "He torture the girl and fuck her so brutally that they tear open both her ass and her pussy.",
+    duration: "1:28:55",
+    categories: ["BS"],
+    preview: "https://candylink69.embed4me.vip/uCeD6eFJFLC11FcKDEVGVQ/mik/dq8zrxeh/95qz5n/preview.webp"
+  },
+    {
+    id: "O002",
+    title: "At the bus stand, right in front of her own grandfather, she starts getting fucked by a stranger.",
+    duration: "35:18",
+    categories: ["O"],
+    preview: "https://candylink69.embed4me.vip/YYv-lzn-cOVjEluKUm1nhA/il/dq8zrxeh/nfvm9u/preview.webp"
+  },
+  {
+    id: "S006",
+    title: "She gets fucked in the ass by her own brother to fulfill her anal fantasy.",
+    duration: "31:05",
+    categories: ["sister"],
+    preview: "https://candylink69.embed4me.vip/0D0BOdszcoZJ8PKvZh2ibQ/s93/dq8zrxeh/qbw6gh/preview.webp"
+  },
+  {
+    id: "S007",
+    title: "Sister's ex-boyfriend fucks her brutally.",
+    duration: "39:22",
+    categories: ["sister"],
+    preview: "https://candylink69.embed4me.vip/RO905At_Yk4iHLBUyP-few/djx/6spnil6i/xoljeg/preview.webp"
+  }
+];
+
