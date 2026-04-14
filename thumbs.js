2b43f5fb1bd0b0630e8b83503c834ff3fa4f24fa
216120306076 <omomshanti079@gmail.com>
2026-04-14 13:28:36 +0530

    Update thumbs.js

--- thumbs.js
+++ thumbs.js
@@ -1,9 +1,12 @@
 const thumbnails = {
-    "V001": "https://i.postimg.cc/B6hp9kbb/V001.webp",
-    "HM001": "https://i.postimg.cc/B6hp9kbb/V001.webp",
-    "HM002": "https://i.postimg.cc/B6hp9kbb/V001.webp",
-    "D001": "https://i.postimg.cc/B6hp9kbb/V001.webp",
-    "Movie004": "https://i.postimg.cc/B6hp9kbb/V001.webp"
+    "B001":  "https://i.postimg.cc/qRVV6md2/B001.webp",
+    "B002":  "https://i.postimg.cc/C1TTnvVG/B002.webp",
+    "BM001": "https://i.postimg.cc/mDff1dG3/BM001.webp",
+    "BS001": "https://i.postimg.cc/HxDD8SCB/BS001.webp",
+    "D": "https://i.postimg.cc/QtGGK6r4/D.webp",
+    "D001":  "https://i.postimg.cc/MT22fs8s/D001.webp",
+    "FF001": "https://i.postimg.cc/KzSS39yw/FF001.webp",
+    "FF002": "https://i.postimg.cc/QtGGK6rn/FF002.webp"
 };
 
 function getThumbnailUrl(videoId) {
